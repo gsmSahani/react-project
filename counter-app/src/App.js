d58a93f1f97d0118ee05a98dd718bb6f7d703b3e
gsmSahani <sahanigautam0446162@gmail.com>
2024-06-22 18:05:13 +0530

    counter app

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+  const decrement = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+  const reload = () => {
+    window.location.reload();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>Counter App</h1>
+        <div className="output-display">
+          <h3>{counter}</h3>
+        </div>
+        <div className="btn">
+          <button className="decrement" onClick={decrement}>
+            -
+          </button>
+          <button className="increment" onClick={increment}>
+            +
+          </button>
+        </div>
+        <div className="reset">
+          <button onClick={reload}>Reset</button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
