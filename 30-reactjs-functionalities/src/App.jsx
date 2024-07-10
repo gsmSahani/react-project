7135777e7308da4551e725a4ba8d0865d488e269
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-10 19:37:22 +0530

    todo application with the feature of only add and remove

--- 30-reactjs-functionalities/src/App.jsx
+++ 30-reactjs-functionalities/src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Toggle from "./Component/Toggle/Toggle";
 import FetchApi from "./Component/FetchApi/FetchApi";
 import Timer from "./Component/Timer/Timer";
+import Todo from "./Component/Todo/Todo";
 
 const App = () => {
   return (
     <>
       {/* <Toggle /> */}
       {/* <FetchApi /> */}
-      <Timer />
+      {/* <Timer /> */}
+      <Todo />
     </>
   );
 };
